4355a5c9fb9e2aedc758d7539b90e2024b2b5297
ni-eminen <matiasnie2@gmail.com>
2022-01-31 02:35:39 +0200

    added project to heroku

---
+++ quizes.sql
@@ -0,0 +1,3 @@
+insert into quizes (name, description, url) values ('sql-quiz', 'This quiz will quiz you on your sql expertise!', 'sql-quiz');
+insert into quizes (name, description, url) values ('generic quiz', 'This quiz will quiz you about something that is yet to be decided...', 'generic-quiz');
+insert into quizes (name, description, url) values ('generic quiz', 'This quiz will quiz you about something that is yet to be decided...', 'generic-quiz');
--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
-CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE quizes (id SERIAL PRIMARY KEY, name TEXT, description TEXT, url TEXT);
+CREATE TABLE questions (id SERIAL PRIMARY KEY, question_string TEXT);
+CREATE TABLE answers (id SERIAL PRIMARY KEY, answer_string TEXT, CONSTRAINT question_id FOREIGN KEY (id) REFERENCES questions (id), is_correct INTEGER);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE scores (id SERIAL PRIMARY KEY, CONSTRAINT user_id FOREIGN KEY (id) REFERENCES users (id), score INTEGER, CONSTRAINT quiz_id FOREIGN KEY (id) REFERENCES quizes (id))
