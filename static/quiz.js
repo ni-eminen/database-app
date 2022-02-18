dee4752cd68dbf61c3d90759bb3a5a5f7b6dd1bd
ni-eminen <matiasnie2@gmail.com>
2022-02-18 17:37:30 +0200

    added header macro

--- static/quiz.js
+++ static/quiz.js
@@ -41,11 +41,4 @@ const submit = async () => {
     window.location.replace(`http://localhost:5000/quiz/results/${response.id}`);
 }
 
-$('.radio-group .radio').click(function(){
-    $(this).parent().find('.radio').removeClass('selected');
-    $(this).addClass('selected');
-    // var val = $(this).attr('data-value');
-    // //alert(val);
-    // $(this).parent().find('input').val(val);
-});
 
